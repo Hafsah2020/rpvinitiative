74ae9b3771467e1a1a5d5a784c6f3b5713a9097f
Hafsah2020 <hafsahoyin@yahoo.com>
2025-05-29 13:32:41 +0100

    navigation

--- js/main.js
+++ js/main.js
@@ -76,3 +76,10 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
